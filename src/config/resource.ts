3b72739db77240899f5912564ac17f741434abcd
lZh1r <electroxhackerpig09@gmail.com>
2025-07-20 18:02:30 +0300

    migrated resource config

---
+++ src/config/resource.ts
@@ -0,0 +1,18 @@
+export const resourceValueConfig = {
+    iron: {
+        ore: 5,
+        ingot: 10
+    },
+    copper: {
+        ore: 3,
+        ingot: 6
+    },
+    coal: {
+        ore: 2,
+        processed: 4
+    }
+};
+
+export const resourceFuelConfig = {
+    coal: 10
+};
