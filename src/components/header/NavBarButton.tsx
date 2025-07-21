f74a64b86cfcfc5be8eb74cf4d2f9bb298594694
lZh1r <electroxhackerpig09@gmail.com>
2025-07-21 21:36:46 +0300

    NavBarButtons now have styling if their respective tab is active

--- src/components/header/NavBarButton.tsx
+++ src/components/header/NavBarButton.tsx
@@ -1,12 +1,24 @@
-import type {JSX} from "react";
-import {gameActions} from "../../game-state.ts";
+import {type JSX, useState} from "react";
+import {currentTab, gameActions} from "../../game-state.ts";
+import {useSignalEffect} from "@preact/signals";
 
 export function NavBarButton({title, correspondingTab}: {title: string, correspondingTab: () => JSX.Element}) {
+
+    const [isActive, setIsActive] = useState(false);
+
+    useSignalEffect(() => {
+        if (correspondingTab === currentTab.value) {
+            setIsActive(true);
+        } else {
+            setIsActive(false);
+        }
+    });
+
     return (
         <button
-            className="bg-card-background p-2 text-center border-2 border-muted-foreground
-                cursor-pointer px-5 max-sm:w-full w-[6em] max-sm:place-self-center shadow-button
-                focus:bg-hover-card-background hover:border-foreground focus:border-foreground focus:shadow-none"
+            className={`bg-card-background p-2 text-center border-2 cursor-pointer px-5 max-sm:w-full 
+                w-[6em] max-sm:place-self-center hover:bg-hover-card-background hover:border-foreground
+                ${isActive ? "border-foreground shadow-none bg-hover-card-background" : "border-muted-foreground shadow-button"}`}
             onClick={() => {
             gameActions.openTab(correspondingTab);
             }}>
