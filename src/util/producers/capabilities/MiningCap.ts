e2e6280e22d9ce37328733fcf78f9e617fa2b9e1
lZh1r <electroxhackerpig09@gmail.com>
2025-07-22 14:22:30 +0300

    mining capability

---
+++ src/util/producers/capabilities/MiningCap.ts
@@ -0,0 +1,20 @@
+import type {IProdCapability} from "./IProdCapability.ts";
+import type {ProducerType} from "../Producer.ts";
+import type {LootTable} from "../../LootTable.ts";
+
+export class MiningCap implements IProdCapability {
+    public id: IDString;
+    public applicableToProducerOfType: ProducerType;
+    constructor(
+        public lootTable: LootTable,
+        public yieldMultiplier: number = 1,
+        public autoSell: boolean = false,
+    ) {
+        this.id = "mining";
+        this.applicableToProducerOfType = "resource";
+    }
+
+    expandLootTable(extraTable: LootTable) {
+        this.lootTable = this.lootTable.combine(extraTable);
+    }
+}
