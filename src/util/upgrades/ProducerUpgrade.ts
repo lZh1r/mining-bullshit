c0965e025012ed6502719a530797691d08b1e8f8
lZh1r <electroxhackerpig09@gmail.com>
2025-07-23 12:00:20 +0300

    producer upgrade class

---
+++ src/util/upgrades/ProducerUpgrade.ts
@@ -0,0 +1,20 @@
+import type {ProducerType} from "../producers/Producer.ts";
+import type {GigaNum} from "../GigaNum.ts";
+import type {Resource} from "../resources/Resource.ts";
+
+export class ProducerUpgrade {
+    public isBought: boolean;
+    constructor(
+        public readonly id: IDString,
+        public readonly name: NameString,
+        public readonly description: string,
+        public readonly type: ProducerType,
+        public effect: () => void,
+        public requirements: [GigaNum, [Resource, number][]],
+        public repeatable: boolean = false,
+        public moneyCostScale: number = 1,
+        public resourceCostScale: number = 1,
+    ) {
+        this.isBought = false;
+    }
+}
