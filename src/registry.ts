977422e414719a97b54de603ad75b1f06d9f1548
lZh1r <electroxhackerpig09@gmail.com>
2025-07-24 12:33:37 +0300

    abolished "config" system as it was too clunky

--- src/registry.ts
+++ src/registry.ts
@@ -1,13 +1,9 @@
 import {Resource} from "./util/resources/Resource";
-import {resourceFuelConfig, resourceValueConfig} from "./config/resource";
-import {BurnableCap} from "./util/resources/capabilities/BurnableCap";
 import {Producer} from "./util/producers/Producer.ts";
 import {EnergyGenCap} from "./util/producers/capabilities/EnergyGenCap.ts";
-import {producerConfig} from "./config/producer.ts";
 import {EnergyConsumptionCap} from "./util/producers/capabilities/EnergyConsumptionCap.ts";
 import {MiningCap} from "./util/producers/capabilities/MiningCap.ts";
 import {LootTable} from "./util/LootTable.ts";
-import {oreLootTableConfig} from "./config/loottable.ts";
 import {ProducerUpgrade} from "./util/upgrades/ProducerUpgrade.ts";
 import {GigaNum} from "./util/GigaNum.ts";
 import {gameActions} from "./game-state.ts";
@@ -15,39 +11,36 @@ import {Recipe} from "./util/crafts/Recipe.ts";
 
 /* RESOURCES */
 /* TIER 1 */
-export const ROCK = new Resource("rock", "Rocks", resourceValueConfig.rock.raw);
-export const IRON_ORE = new Resource("iron_ore", "Iron Ore", resourceValueConfig.iron.ore);
-export const IRON_INGOT = new Resource("iron_ingot", "Iron Ingot", resourceValueConfig.iron.ingot);
-export const COPPER_ORE = new Resource("copper_ore", "Copper Ore", resourceValueConfig.copper.ore);
-export const COPPER_INGOT = new Resource("copper_ingot", "Copper Ingot", resourceValueConfig.copper.ingot);
-export const COAL_ORE = new Resource("coal_ore", "Coal Ore", resourceValueConfig.coal.ore);
-const COAL = new Resource("coal", "Coal", resourceValueConfig.coal.processed);
-COAL.addCapability(new BurnableCap(resourceFuelConfig.coal));
+export const ROCK = new Resource("rock", "Rocks", 1);
+export const IRON_ORE = new Resource("iron_ore", "Iron Ore", 5);
+export const IRON_INGOT = new Resource("iron_ingot", "Iron Ingot", 15);
+export const COPPER_ORE = new Resource("copper_ore", "Copper Ore", 3);
+export const COPPER_INGOT = new Resource("copper_ingot", "Copper Ingot", 9);
+export const COAL_ORE = new Resource("coal_ore", "Coal Ore", 2);
+const COAL = new Resource("coal", "Coal", 6);
 
 /* LOOT TABLES */
 export const MINING_TIER1 = new LootTable([
-    [ROCK, oreLootTableConfig.planetA.mining.tier1.rock],
-    [COPPER_ORE, oreLootTableConfig.planetA.mining.tier1.copper],
-    [COAL_ORE, oreLootTableConfig.planetA.mining.tier1.coal],
-    [IRON_ORE, oreLootTableConfig.planetA.mining.tier1.iron]
+    [ROCK, 10],
+    [COPPER_ORE, 3],
+    [COAL_ORE, 5],
+    [IRON_ORE, 2]
 ]);
 
 /* PRODUCERS */
 /* ENERGY */
 export const HAMSTER_WHEEL = Producer.energy("hamster_wheel", "Hamster Wheel",
-    producerConfig.energy.hamsterWheel.defaultCost, producerConfig.energy.hamsterWheel.defaultCostScale);
-HAMSTER_WHEEL.addCapability(new EnergyGenCap(producerConfig.energy.hamsterWheel.energyGeneration));
-HAMSTER_WHEEL.addMilestone(1, () => {console.log("I was triggered");});
+    new GigaNum(5), new GigaNum(1.5));
+HAMSTER_WHEEL.addCapability(new EnergyGenCap(new GigaNum(5)));
 /* RESOURCE */
 export const MINE = Producer.resource("mine", "Mine",
-    producerConfig.resource.mine.defaultCost, producerConfig.resource.mine.defaultCostScale, producerConfig.resource.mine.ticksPerOperation);
-MINE.addCapability(new EnergyConsumptionCap(producerConfig.resource.mine.energyConsumption));
+    new GigaNum(20), new GigaNum(2.5), 5);
+MINE.addCapability(new EnergyConsumptionCap(new GigaNum(10)));
 MINE.addCapability(new MiningCap(MINING_TIER1));
 /* CRAFTING */
 export const FURNACE = Producer.crafting("furnace", "Furnace",
-    producerConfig.crafting.furnace.defaultCost, producerConfig.crafting.furnace.defaultCostScale,
-    producerConfig.crafting.furnace.ticksPerOperation, 1, [[ROCK, 10]]);
-FURNACE.addCapability(new EnergyConsumptionCap(producerConfig.crafting.furnace.energyConsumption));
+    new GigaNum(30), new GigaNum(2), 2, 1, [[ROCK, 10]]);
+FURNACE.addCapability(new EnergyConsumptionCap(new GigaNum(15)));
 
 /* RECIPES */
 const IRON_INGOT_FURNACE = new Recipe("iron_ingot_furnace", "Iron Ore to Ingot",
