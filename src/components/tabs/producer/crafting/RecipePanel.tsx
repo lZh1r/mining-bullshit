66ebffefea66ba849a7243daa6a419358773009b
lZh1r <electroxhackerpig09@gmail.com>
2025-07-26 11:49:50 +0300

    i hate overflows

--- src/components/tabs/producer/crafting/RecipePanel.tsx
+++ src/components/tabs/producer/crafting/RecipePanel.tsx
@@ -5,17 +5,16 @@ export function RecipePanel() {
     return (
         <div className="bg-card-background border-2 border-muted-foreground col-span-2 mr-2
          [&::-webkit-scrollbar]:w-3 [&::-webkit-scrollbar-track]:bg-card-background
-         [&::-webkit-scrollbar-thumb]:bg-muted-foreground [&::-webkit-scrollbar-thumb]:w-3 overflow-x-hidden overflow-scroll">
+         [&::-webkit-scrollbar-thumb]:bg-muted-foreground [&::-webkit-scrollbar-thumb]:w-3 overflow-x-hidden overflow-scroll max-h-[84vh]">
             <h1 className="text-3xl text-center p-2 border-b-2 border-b-muted-foreground">Recipes</h1>
             {
                 Array.from(recipes.value).map(([producer, recipes]) =>
-                    <div className="flex flex-col max-h-[78vh]"
-                         key={`${producer.id}_recipes`}>
+                    <>
                         <h2 className="text-2xl text-center">{producer.name}</h2>
                         {recipes.map(recipe =>
                             <RecipeCard recipe={recipe}/>
                         )}
-                    </div>
+                    </>
                 )
             }
         </div>
