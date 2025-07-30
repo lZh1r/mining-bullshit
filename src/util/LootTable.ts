5a79bc27e6927171ae33448880c20241010a19ae
lZh1r <electroxhackerpig09@gmail.com>
2025-07-30 13:03:07 +0300

    a little loottable rework

--- src/util/LootTable.ts
+++ src/util/LootTable.ts
@@ -7,11 +7,15 @@ interface LootInterval {
 }
 
 export class LootTable {
-    private readonly totalWeight: number;
-    private lootLookUpTable: LootInterval[];
+    private totalWeight: number = 0;
+    private lootLookUpTable: LootInterval[] = [];
     constructor(
         private resourceWeightPairs: [Resource, number][]
     ) {
+        this.generateLootLookUpTable(resourceWeightPairs);
+    }
+
+    private generateLootLookUpTable(resourceWeightPairs: [Resource, number][]) {
         let totalWeight = 0;
         let prevMin = 0;
         const lootLookUpTable = new Array<LootInterval>;
@@ -29,10 +33,11 @@ export class LootTable {
     }
 
     roll(n: number = 1): [Resource, number][] {
-        const outcome = new Map<Resource, number>;
+        const outcome = new Map<Resource, number>();
         for (let i = 0; i < n; i++) {
             const rolledNumber = Math.floor(Math.random() * this.totalWeight);
-            const resourceToAdd = this.lootLookUpTable.find(entry => rolledNumber >= entry.min && rolledNumber < entry.max)!.value;
+            const resourceToAdd =
+                this.lootLookUpTable.find(entry => rolledNumber >= entry.min && rolledNumber < entry.max)!.value;
             const prevCount = outcome.get(resourceToAdd) ?? 0;
             outcome.set(resourceToAdd, prevCount + 1);
         }
@@ -40,10 +45,39 @@ export class LootTable {
     }
 
     combine(...tables: LootTable[]): LootTable {
-        let newResourceWeightPairs = this.resourceWeightPairs;
+        const newResourceWeightPairs = this.resourceWeightPairs;
         for (const table of tables) {
-            newResourceWeightPairs = newResourceWeightPairs.concat(table.resourceWeightPairs);
+            for (const [resource, weight] of table.resourceWeightPairs) {
+                const existingPair =
+                    newResourceWeightPairs.find((val) => val[0] === resource);
+                if (existingPair) {
+                    newResourceWeightPairs[newResourceWeightPairs.indexOf(existingPair)] = [existingPair[0], existingPair[1] + weight];
+                } else {
+                    newResourceWeightPairs.push([resource, weight]);
+                }
+            }
         }
         return new LootTable(newResourceWeightPairs);
+    }
+
+    getResourceWeightPairs() {
+        return this.resourceWeightPairs;
+    }
+
+    expand(...tables: LootTable[]): void {
+        const newResourceWeightPairs = this.resourceWeightPairs;
+        for (const table of tables) {
+            for (const [resource, weight] of table.resourceWeightPairs) {
+                const existingPair =
+                    newResourceWeightPairs.find((val) => val[0] === resource);
+                if (existingPair) {
+                    newResourceWeightPairs[newResourceWeightPairs.indexOf(existingPair)] = [existingPair[0], existingPair[1] + weight];
+                } else {
+                    newResourceWeightPairs.push([resource, weight]);
+                }
+            }
+        }
+        this.resourceWeightPairs = newResourceWeightPairs;
+        this.generateLootLookUpTable(newResourceWeightPairs);
     }
 }
